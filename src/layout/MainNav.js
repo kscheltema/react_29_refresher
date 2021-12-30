d1c48b78a0dc7a723c63e65ae9ab2be7441bbbee
kscheltema <k87scheltema@outlook.com>
2021-12-30 19:19:01 +0200

    original JSX

---
+++ src/layout/MainNav.js
@@ -0,0 +1,16 @@
+function MainNav() {
+  return (
+    <header>
+      <div>React Meetups</div>
+      <nav>
+        <ul>
+          <li>
+            <a href="">Some Link</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default MainNav;
