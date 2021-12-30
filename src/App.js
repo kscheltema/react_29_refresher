229b4015d2563e8b6754312050f207064a1ae62e
kscheltema <k87scheltema@outlook.com>
2021-12-30 20:40:28 +0200

    CSS modules alternative to Material UI

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import { Route, Routes } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import FavPage from "./pages/Fav";
 import NewMeetupsPage from "./pages/NewMeetups";
+import MainNav from "./layout/MainNav";
 
 function App() {
   return (
     <div>
+      <MainNav />
       <Routes>
         <Route path="/" exact element={<AllMeetupsPage />} />
         <Route path="/new-meetup" element={<NewMeetupsPage />} />
--- src/layout/MainNav.js
+++ src/layout/MainNav.js
@@ -1,13 +1,20 @@
 import { Link } from "react-router-dom";
+import classes from "./MainNav.module.css";
 
 function MainNav() {
   return (
-    <header>
-      <div>React Meetups</div>
+    <header className={classes.header}>
+      <div className={classes.logo}>React Meetups</div>
       <nav>
         <ul>
           <li>
-            <Link href="">Some Link</Link>
+            <Link to="/">All Meetups</Link>
+          </li>
+          <li>
+            <Link to="/new-meetup">New Meetups</Link>
+          </li>
+          <li>
+            <Link to="/favorite">My Favorites</Link>
           </li>
         </ul>
       </nav>
