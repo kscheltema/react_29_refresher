488886f4951c260f0c3c447c32a3d62adc9c97b8
kscheltema <k87scheltema@outlook.com>
2022-01-01 11:08:31 +0200

    send data to a server

--- src/pages/NewMeetups.js
+++ src/pages/NewMeetups.js
@@ -1,10 +1,14 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupsPage() {
+  function addMeetupHandler(meetupData) {
+    //send a http request here
+  }
+
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 }
