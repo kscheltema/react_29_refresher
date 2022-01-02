5cec5d7fd747e75948c9fca01a5e47867acf7441
kscheltema <k87scheltema@outlook.com>
2022-01-02 11:28:46 +0200

    useNavigate replaces useHistory
    double back still working

--- src/pages/NewMeetups.js
+++ src/pages/NewMeetups.js
@@ -1,6 +1,9 @@
+import { useNavigate } from "react-router-dom";
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupsPage() {
+  const navigate = useNavigate();
+
   function addMeetupHandler(meetupData) {
     fetch(
       //fetch default a get request
@@ -10,7 +13,9 @@ function NewMeetupsPage() {
         body: JSON.stringify(meetupData),
         headers: { "Content-Type": "application/json" },
       } //this changes the default method to the quote method
-    );
+    ).then(() => {
+      navigate("/", { replace: true });
+    });
   }
 
   return (
